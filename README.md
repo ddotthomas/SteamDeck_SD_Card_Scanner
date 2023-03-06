352e35e999fc81827e33cbc159b766df9f4981c2
ddotthomas <ddotthomas@gmail.com>
2023-03-06 16:39:53 -0700

    Added Non Steam library support
    Also fixed a couple bugs and made the UI easier to read on the Deck
    Still haven't fixed the mouse being able to move to the bottom right
    Changed the iced app from being a Sandbox to an Application

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 <img alt="SD Card Scanner" src="https://i.imgur.com/NJYFN3t.jpg">
 
-### How to use:
+## How to use:
 
 - Switch to Desktop mode to download and install
 - Download the pre-compiled binary from the [Releases](https://github.com/ddotthomas/SteamDeck_SD_Card_Scanner/releases) or compile your own
@@ -15,23 +15,40 @@
 
 Then when you launch the program, it will scan the current inserted SD Card and add it and all its games to the list. You can switch SD Cards and restart the program or hit Scan Card to force a scan of the current card, the program will detect them as different cards and keep track of which one has which games.
 
-### Things to be added or improved/ known issues
+## Non Steam Libraries:
+
+The app will scan the SD card for any folders with the words "lutris" or "heroic" case-insensitive. If it doesn't find any folders with either of those words it will look for any folders with "other" or "games" instead and search either of those folders for another folder with "lutris" or "heroic" inside them.
+
+```
+example folders
+sd/lutris/
+sd/heroic/
+sd/other games/lutris/
+sd/other games/heroic/
+```
+
+## Things to be added or improved/ known issues
 
 - There's currently an issue where it looks like gamescope sees the application at a different resolution than it actually is, more info [here](https://www.reddit.com/r/SteamDeck/comments/10jk36q/having_issues_with_the_windows_size_while/). I recommend switching the controller layout to the "Web Browser" scheme provided by Valve. Otherwise, the mouse can't make it to the scroll bar on the right side to scroll down.
 - Let the program scan and add Non-Steam Games to the list. (Lutris and Heroic)
 - Allow the user to search for a game, filtering the list
 - Add a settings page, allow the user some control over how the cards are identified, scanned, and more
 - Make a better UI
-- Cards are named automatically and the system for picking a number isn't perfect
+- Improve the Non Steam library folder scanning to scan any previously found folders
 
 ## How to Compile
 
 - First, download Rust, I recommend using [Rustup](https://www.rust-lang.org/tools/install)
 - Update Rust ```rustup update```
+- Install other [dependencies](#dependencies), for Ubuntu/Debian based distros: ```sudo apt install libfontconfig-dev```
 - Clone the repository, ```git clone https://github.com/ddotthomas/SteamDeck_SD_Card_Scanner```
 - Switch to the new Directory and install ```cd SteamDeck_SD_Card_Scanner; cargo build --release```
 - Look for the binary in the ```target/release``` directory titled "steamdeck_sd_card_scanner"
 - I recommend moving the binary to ```~/.local/bin/``` but you can leave it anywhere you want when you add it as a Non-Steam Game
+
+## Dependencies 
+
+ - Part of iced requires the library fontconfig to be installed, on Ubuntu that package name is libfontconfig-dev. 
 
 ## Support
 
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-use dirs;
 use iced::widget::{button, column, container, row, scrollable, text};
-use iced::widget::{Button, Column, Container};
-use iced::{window, Alignment, Color, Element, Length, Renderer, Sandbox, Settings, Theme};
-use std::{fs, path::PathBuf, process};
+use iced::widget::Column;
+use iced::{
+    executor, window, Alignment, Application, Command, Element, Length, Settings, Theme,
+};
+use std::process;
 
 mod scanning;
 
 use scanning::{Card, Game};
 
-
-
 fn main() -> iced::Result {
-    let mut size = (1280, 800);
+    let size = (1280, 800);
     App::run(Settings {
-        window: window::Settings { 
+        window: window::Settings {
             size,
-            max_size: Some((1280,800)),
+            // max_size: Some((1280, 800)),
             ..window::Settings::default()
         },
         ..Settings::default()
@@ -27,23 +26,27 @@ pub struct App {
     current: usize,
 }
 
-impl Sandbox for App {
+impl Application for App {
     type Message = Message;
+    type Executor = executor::Default;
+    type Theme = Theme;
+    type Flags = ();
 
-    fn new() -> App {
-        App {
-            pages: vec![
-                Page::List(get_card_data()),
-            ],
-            current: 0,
-        }
+    fn new(_flags: ()) -> (App, iced::Command<Message>) {
+        (
+            App {
+                pages: vec![Page::List(get_card_data())],
+                current: 0,
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
         format!("Steam Deck SD Card Scanner")
     }
 
-    fn update(&mut self, event: Message) {
+    fn update(&mut self, event: Message) -> Command<Self::Message> {
         match event {
             Message::ClearList => {
                 Page::clear_list(&mut self.pages[self.current]);
@@ -53,21 +56,51 @@ impl Sandbox for App {
             }
             Message::ScanCard => Page::scan_card(&mut self.pages[self.current]),
             Message::Exit => process::exit(0),
+            Message::Fullscreen => return window::toggle_maximize(),
         }
+
+        Command::none()
     }
 
     fn view(&self) -> Element<Message> {
-        let mut controls = iced::widget::column(vec![
-            text("Steam Deck\nSD Card Scanner").size(25).into(),
-            button("Scan Card").on_press(Message::ScanCard).into(),
-            // button("Test List").on_press(Message::TestList).into(),
-            // button("Clear List").on_press(Message::ClearList).into(),
-            button("Exit").on_press(Message::Exit).into(),
+        let controls = column(vec![
+            container(text("Steam Deck\nSD Card Scanner").size(50))
+                .padding(10)
+                .into(),
+            container(
+                button(text("Rescan Card").size(33))
+                    .padding(12)
+                    .on_press(Message::ScanCard),
+            )
+            .padding(10)
+            .into(),
+            //container(button(text("Test List").size(33)).padding(12).on_press(Message::TestList))
+            //    .padding(10)
+            //    .into(),
+            //container(
+            //    button(text("Clear List").size(33))
+            //        .padding(12)
+            //        .on_press(Message::ClearList),
+            //)
+            //.padding(10)
+            //.into(),
+            container(button(text("Exit").size(33)).padding(12).on_press(Message::Exit))
+                .padding(10)
+                .into(),
+            //container(
+            //    button(text("Fullscreen").size(33))
+            //        .padding(12)
+            //        .on_press(Message::Fullscreen),
+            //)
+            //.padding(10)
+            //.into(),
         ])
-        .padding(20)
+        .padding(12)
         .align_items(Alignment::Center);
+
         let content = self.pages[self.current].view();
-        container(row!(controls, scrollable(content))).into()
+
+        container(row!(controls, scrollable(content))).width(Length::Fill).height(Length::Fill).into()
     }
 
     fn theme(&self) -> Theme {
@@ -81,6 +114,7 @@ pub enum Message {
     TestList,
     ScanCard,
     Exit,
+    Fullscreen,
 }
 
 pub enum Page {
@@ -111,6 +145,8 @@ impl<'a> Page {
                         name: String::from("Game Two"),
                     },
                 ],
+                lutris: None,
+                heroic: None,
             }),
         }
     }
@@ -128,44 +164,86 @@ impl<'a> Page {
     }
 }
 
+/// Creates the list of cards and their games underneath each one for the iced GUI
 fn create_card_and_games_list(list: &Vec<Card>) -> Vec<Element<Message>> {
-    // Creates the list of cards and their games underneath each one for the iced GUI
     let mut return_list: Vec<Element<Message>> = vec![];
 
     for card in list {
-        return_list.push(text(format!("Card: {}", card.name)).size(30).into());
+        return_list.push(text(format!("Card: {}", card.name)).size(50).into());
 
-        let game_list =
+        // Label the Steam Library if there's also Non Steam Libraries
+        if card.heroic.is_some() || card.lutris.is_some() {
+            return_list.push(text(format!("Steam Games")).size(40).into())
+        }
+
+        return_list.push(
             card.games
                 .iter()
                 .fold(column![], |column: Column<Message>, game: &Game| {
-                    column.push(text(format!("Game: {}", game.name)))
-                });
+                    column.push(text(format!("{}", game.name)).size(30))
+                })
+                .into(),
+        );
 
-        return_list.push(game_list.into())
+        if card.lutris.is_some() {
+            return_list.push(text(format!("Lutris Library")).size(40).into());
+
+            if let Some(library) = &card.lutris {
+                return_list.push(
+                    library
+                        .games
+                        .iter()
+                        .fold(column![], |column: Column<Message>, game: &Game| {
+                            column.push(text(format!("{}", game.name)).size(30))
+                        })
+                        .into(),
+                )
+            }
+        }
+
+        if card.heroic.is_some() {
+            return_list.push(text(format!("Heroic Library")).size(40).into());
+
+            if let Some(library) = &card.heroic {
+                return_list.push(
+                    library
+                        .games
+                        .iter()
+                        .fold(column![], |column: Column<Message>, game: &Game| {
+                            column.push(text(format!("{}", game.name)).size(30))
+                        })
+                        .into(),
+                )
+            }
+        }
     }
+
+    // return_list.push(text("For some reason the either the iced-rs backend or Gamescope doesn't allow the mouse to move down here.\nIf you know why or can help at all consider contributing on GitHub").vertical_alignment(alignment::Vertical::Bottom).size(15).into());
 
     return_list
 }
 
+/// Gets the saved data from the json file in ~/.config and updates it with the currently inserted card
 fn get_card_data() -> Vec<Card> {
     let list: Vec<Card> = match scanning::get_saved_data() {
-        // Check for a json file save, add the data from it
         Some(mut list) => {
+            // Update the current cards data to the list, update the file
             scanning::add_current_card(&mut list);
+            scanning::save_data(&list);
             list
-        } // Update the current cards data to the list, update the file
+        }
         None => match scanning::scan_card(None) {
             // If the save file wasn't found, scan the current card if it exists and create the save file
-            Some(card) => vec![card],
+            Some(card) => {
+                scanning::save_data(&vec![card.clone()]);
+                vec![card]
+            }
             None => {
                 eprintln!("Error scanning SD card data");
                 vec![]
-            },
+            }
         },
     };
 
     list
 }
-
-
