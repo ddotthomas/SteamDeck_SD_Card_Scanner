88afa256d5fe755fa9145232728afaa8ecbe3f9f
ddotthomas <ddotthomas@gmail.com>
2023-10-09 11:53:26 -0600

    Added Settings Page
    Has some information about the different saved cards.
    Allows the user to change the card name

---
+++ src/app/pages.rs
@@ -0,0 +1,79 @@
+use crate::app::utils;
+use crate::app::utils::{long_settings_label, settings_label};
+use crate::app::Message;
+use crate::scanning::Card;
+use iced::widget::{column, container, row, text, text_input, Column};
+use iced::{Element, Length};
+
+pub enum Page {
+    List,
+    Settings,
+}
+
+impl<'a> Page {
+    /// view() probably shouldn't be designed to require card_data or search_term but it works for now
+    pub fn view(&'a self, card_data: &'a Vec<Card>, search_term: &'a str) -> Element<Message> {
+        match self {
+            Page::List => Self::list(card_data, search_term).into(),
+            Page::Settings => Self::settings(card_data).into(),
+        }
+    }
+
+    fn list(list: &'a Vec<Card>, search_term: &'a str) -> Column<'a, Message> {
+        let mut element_list: Vec<Element<Message>> = vec![container(row(vec![
+            text_input("Filter Search...", search_term, |text_value| {
+                Message::SearchInput(text_value)
+            })
+            .size(30)
+            .width(Length::FillPortion(2))
+            .padding(4)
+            .into(),
+            container(
+                text(format!(
+                    "Current Card: {}",
+                    if let Some(card_name) = utils::get_card_name(list) {
+                        card_name
+                    } else {
+                        String::from("No Card Detected")
+                    }
+                ))
+                .size(30),
+            )
+            .padding(2)
+            .width(Length::FillPortion(3))
+            .into(),
+        ]))
+        .padding(4)
+        .into()];
+
+        element_list.push(utils::create_card_and_games_list(list, search_term).into());
+        column(element_list).width(Length::Fill)
+    }
+
+    // TODO
+    fn settings(list_data: &Vec<Card>) -> Column<Message> {
+        let mut element_list: Vec<Element<Message>> = vec![
+            container(text("Settings").size(40)).padding(2).into(),
+            row(vec![
+                settings_label(text("ID").size(25)),
+                settings_label(text("Game(s)").size(25)),
+                long_settings_label(text("Card Name").size(25)),
+            ])
+            .into(),
+        ];
+
+        for card in list_data {
+            let card_settings = row(vec![
+                settings_label(text(&card.uuid[..4])),
+                settings_label(text(format!("{}", utils::card_games_count(&card)))),
+                long_settings_label(text_input(&card.name, &card.name, |user_input| {
+                    Message::ChangeCardName(user_input, card.uuid.clone())
+                })),
+            ]);
+
+            element_list.push(card_settings.into())
+        }
+
+        column(element_list)
+    }
+}
--- src/app/utils.rs
+++ src/app/utils.rs
@@ -1,4 +1,8 @@
-use crate::scanning::{Card, OtherLibrary};
+use crate::app::theming;
+use crate::app::Message;
+use crate::scanning::{save_data_to_json, Card, Game, OtherLibrary};
+use iced::widget::{button, column, container, scrollable, text, Column, Scrollable};
+use iced::{Element, Length};
 
 /// Returns a copy of the passed in list after it's been filtered by the search term
 pub fn filter_list(list: &Vec<Card>, search_term: &str) -> Vec<Card> {
@@ -64,4 +68,227 @@ pub fn get_card_name(list: &Vec<Card>) -> Option<String> {
         .map(|card| card.name.clone())
         .next()
     // Get the next item which should be the one inserted card
+}
+
+/// Converts the list data into an Iced GUI list of the cards and their games
+/// Also provides the search functionality by filtering the list data by the `search_term`
+/// The `search_term` is is provided by the user in search bar
+pub fn create_card_and_games_list<'a>(
+    list: &'a Vec<Card>,
+    search_term: &'a str,
+) -> Scrollable<'a, Message> {
+    let mut return_list: Vec<Element<Message>> = vec![];
+
+    let list = if search_term.is_empty() {
+        // If the search term is empty, don't filter the list
+        list.clone()
+    } else {
+        filter_list(list, search_term)
+    };
+
+    for card in list {
+        // List is seperated by different SD cards
+
+        // If the lists of games on the card are empty and the search_term isn't empty
+        // We'll assume the card has been 'filtered out' and wont display it at all
+        if card.games.is_empty()
+            && match &card.heroic {
+                None => true,
+                Some(heroic_list) => heroic_list.games.is_empty(),
+            }
+            && match &card.lutris {
+                None => true,
+                Some(lutris_list) => lutris_list.games.is_empty(),
+            }
+            && !search_term.is_empty()
+        {
+            continue;
+        }
+
+        return_list.push(text(format!("Card: {}", card.name)).size(50).into());
+
+        if !card.games.is_empty() {
+            if card.heroic.is_some() || card.lutris.is_some() {
+                // Label the Steam Library if there's also Non Steam Libraries
+                return_list.push(
+                    container(
+                        text(format!("Steam Games"))
+                            .style(theming::STEAM_COLOR)
+                            .size(40),
+                    )
+                    .into(),
+                );
+                return_list.push(
+                    container(text(""))
+                        .width(Length::Units(theming::DIVIDER_BAR_LENGTH))
+                        .height(Length::Units(theming::DIVIDER_BAR_HEIGHT))
+                        .padding(4)
+                        .style(theming::STEAM_CONTAINER_STYLE)
+                        .into(),
+                );
+            }
+
+            return_list.push(
+                //
+                card.games
+                    .iter()
+                    .fold(column![], |column: Column<Message>, game: &Game| {
+                        column.push(text(format!("{}", game.name)).size(30))
+                    })
+                    .into(),
+            );
+        }
+
+        if let Some(library) = card.lutris {
+            if !library.games.is_empty() {
+                // If there were no games found or they were all filtered out by the search,
+                // don't add any elements to differentiate the other libraries
+                return_list.push(
+                    text(format!("Lutris Library"))
+                        .style(theming::LUTRIS_COLOR)
+                        .size(40)
+                        .into(),
+                );
+
+                // Add a divider line under the Lutris Library label
+                return_list.push(
+                    container(text(""))
+                        .width(Length::Units(theming::DIVIDER_BAR_LENGTH))
+                        .height(Length::Units(theming::DIVIDER_BAR_HEIGHT))
+                        .padding(4)
+                        .style(theming::LUTRIS_CONTAINER_STYLE)
+                        .into(),
+                );
+                return_list.push(
+                    library
+                        .games
+                        .iter()
+                        .fold(column![], |column: Column<Message>, game: &Game| {
+                            column.push(text(format!("{}", game.name)).size(30))
+                        })
+                        .into(),
+                );
+            }
+        }
+
+        if let Some(library) = card.heroic {
+            if !library.games.is_empty() {
+                return_list.push(
+                    text(format!("Heroic Library"))
+                        .style(theming::HEROIC_COLOR)
+                        .size(40)
+                        .into(),
+                );
+                return_list.push(
+                    container(text(""))
+                        .width(Length::Units(theming::DIVIDER_BAR_LENGTH))
+                        .height(Length::Units(theming::DIVIDER_BAR_HEIGHT))
+                        .padding(4)
+                        .style(theming::HEROIC_CONTAINER_STYLE)
+                        .into(),
+                );
+                return_list.push(
+                    library
+                        .games
+                        .iter()
+                        .fold(column![], |column: Column<Message>, game: &Game| {
+                            column.push(text(format!("{}", game.name)).size(30))
+                        })
+                        .into(),
+                );
+            }
+        }
+    }
+
+    scrollable(column(return_list).width(Length::Fill))
+}
+
+/// Gets the saved data from the json file in ~/.config and updates it with the currently inserted card
+pub fn get_card_data() -> Vec<Card> {
+    let list: Vec<Card> = match crate::scanning::get_saved_data() {
+        Some(mut list) => {
+            // Update the current cards data to the list, update the file
+            crate::scanning::add_current_card(&mut list);
+            crate::scanning::save_data_to_json(&list);
+            list
+        }
+        None => match crate::scanning::scan_card(None) {
+            // If the save file wasn't found, scan the current card if it exists and create the save file
+            Some(card) => {
+                crate::scanning::save_data_to_json(&vec![card.clone()]);
+                vec![card]
+            }
+            None => {
+                eprintln!("Error scanning SD card data");
+                // If there wasn't any saved data found in ~/.config/sdscannerssave.json and no current SD card, return an empty list
+                vec![]
+            }
+        },
+    };
+
+    list
+}
+
+pub fn control_button(label: &str, message: Message) -> Element<Message> {
+    container(button(text(label).size(33)).padding(12).on_press(message))
+        .padding(4)
+        .into()
+}
+
+pub fn change_card_name(card_name: String, card_uuid: String, cards: &Vec<Card>) -> Vec<Card> {
+    let mut return_list: Vec<Card> = vec![];
+
+    for card in cards {
+        // For all the cards in the passed in saved list -
+        if card.uuid == card_uuid {
+            // Check if the current card matches the uuid of the card who's name were trying to change
+            // Push a clone card but with the name changed to the new passed in name
+            return_list.push(Card {
+                uuid: card.uuid.clone(),
+                name: card_name.clone(),
+                games: card.games.clone(),
+                heroic: card.heroic.clone(),
+                lutris: card.lutris.clone(),
+            })
+        } else {
+            // Otherwise push the original card to the return list
+            return_list.push(card.clone())
+        }
+    }
+
+    save_data_to_json(&return_list);
+
+    return_list
+}
+
+pub fn settings_label<'a>(content: impl Into<Element<'a, Message>>) -> Element<'a, Message> {
+    container(content)
+        .padding(5)
+        .style(theming::SETTINGS_LABEL_CONTAINER_STYLE)
+        .width(Length::Units(90))
+        .height(Length::Units(60))
+        .into()
+}
+
+pub fn long_settings_label<'a>(content: impl Into<Element<'a, Message>>) -> Element<'a, Message> {
+    container(content)
+        .padding(5)
+        .style(theming::SETTINGS_LABEL_CONTAINER_STYLE)
+        .width(Length::Units(310))
+        .height(Length::Units(60))
+        .into()
+}
+
+pub fn card_games_count(card: &Card) -> usize {
+    let mut count = card.games.len();
+
+    if let Some(heroic) = &card.heroic {
+        count += heroic.games.len();
+    }
+
+    if let Some(lutris) = &card.lutris {
+        count += lutris.games.len();
+    }
+
+    count
 }
